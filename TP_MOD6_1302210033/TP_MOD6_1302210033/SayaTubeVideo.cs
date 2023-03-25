146f4c57bbc6587273becb656ca3426483e6e725
raliauliya <121374492+raliauliya@users.noreply.github.com>
2023-03-26 04:31:18 +0700

    menambahkan class SayaTubeVideo

---
+++ TP_MOD6_1302210033/TP_MOD6_1302210033/SayaTubeVideo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        //menggunakan class random
+  
+        Random r = new Random();
+
+        this.title = title;
+
+        //sepanjang 5 digit
+
+        id = r.Next(10000, 99999);
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int playCount)
+    {
+        this.playCount += playCount;
+
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Details video : ");
+        Console.WriteLine("ID : " + id);
+        Console.WriteLine("Title : " + title);
+        Console.WriteLine("Play Count : " + playCount);
+    }
+}
